812ab36bd6d8820bae2154e4e593bc5403894306
jason13official <135896906+jason13official@users.noreply.github.com>
2024-06-15 22:57:26 -0400

    Update Constants.java

--- common/src/main/java/com/cursee/monolib/Constants.java
+++ common/src/main/java/com/cursee/monolib/Constants.java
@@ -7,5 +7,7 @@ public class Constants {
 
 	public static final String MOD_ID = "monolib";
 	public static final String MOD_NAME = "MonoLib";
+	public static final String MOD_VERSION = "1.3.0";
+	public static final String MC_VERSION_RAW = "[1.20.1]";
 	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 }
