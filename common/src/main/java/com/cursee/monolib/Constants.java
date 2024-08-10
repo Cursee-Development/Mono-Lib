303cfc5ea0bd2ff691a61f076accf638a6fc3f0e
jason13official <jason13official@gmail.com>
2024-08-09 22:54:41 -0400

    port to 1.21.1

--- common/src/main/java/com/cursee/monolib/Constants.java
+++ common/src/main/java/com/cursee/monolib/Constants.java
@@ -8,6 +8,6 @@ public class Constants {
 	public static final String MOD_ID = "monolib";
 	public static final String MOD_NAME = "MonoLib";
 	public static final String MOD_VERSION = "1.3.0";
-	public static final String MC_VERSION_RAW = "[1.21]";
+	public static final String MC_VERSION_RAW = "[1.21.1]";
 	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 }
