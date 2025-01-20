1d0dfdc0c2c3b4726b6ec13b436c3dc6560563e1
jason13official <jason13official@gmail.com>
2025-01-20 16:43:48 -0500

    Update ModItems.java

--- common/src/main/java/com/cursee/monolib/core/registry/ModItems.java
+++ common/src/main/java/com/cursee/monolib/core/registry/ModItems.java
@@ -10,13 +10,14 @@ import java.util.function.BiConsumer;
 
 public class ModItems {
 
+    // must be registered if defined
     // public static final Item EXAMPLE_BLOCK_ITEM = new BlockItem(ModBlocks.EXAMPLE_BLOCK, new Item.Properties()); // if we want the BlockItem separate
 
     public static void register(BiConsumer<Item, ResourceLocation> consumer) {
 
         if (Services.PLATFORM.isDevelopmentEnvironment()) {
             // consumer.accept(EXAMPLE_BLOCK_ITEM, BuiltInRegistries.BLOCK.getKey(ModBlocks.EXAMPLE_BLOCK)); // if we want the BlockItem separate
-            consumer.accept(new BlockItem(ModBlocks.EXAMPLE_BLOCK, new Item.Properties()), BuiltInRegistries.BLOCK.getKey(ModBlocks.EXAMPLE_BLOCK));
+            // consumer.accept(new BlockItem(ModBlocks.EXAMPLE_BLOCK, new Item.Properties()), BuiltInRegistries.BLOCK.getKey(ModBlocks.EXAMPLE_BLOCK));
         }
     }
 }
