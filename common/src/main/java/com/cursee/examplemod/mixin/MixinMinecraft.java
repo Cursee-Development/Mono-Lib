a3285c572930870810e06565a1810584c1090037
jason13official <135896906+jason13official@users.noreply.github.com>
2025-01-20 13:19:49 -0500

    Initialize 1.21.1

---
+++ common/src/main/java/com/cursee/examplemod/mixin/MixinMinecraft.java
@@ -0,0 +1,23 @@
+package com.cursee.examplemod.mixin;
+
+import com.cursee.examplemod.Constants;
+import com.cursee.examplemod.platform.Services;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Minecraft.class)
+public class MixinMinecraft {
+    
+    @Inject(at = @At("TAIL"), method = "<init>")
+    private void init(CallbackInfo info) {
+
+        if (Services.PLATFORM.isDevelopmentEnvironment()) {
+            Constants.LOG.info("This line is printed by an example mod common mixin!");
+            Constants.LOG.info("MC Version: {}", Minecraft.getInstance().getVersionType());
+        }
+    }
+}
