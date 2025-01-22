c137d4d8362ea9d71213a1bba7f1f7384fd14565
jason13official <jason13official@gmail.com>
2025-01-22 16:42:12 -0500

    Create MonoLibConfiguration.java

---
+++ common/src/main/java/com/cursee/monolib/core/MonoLibConfiguration.java
@@ -0,0 +1,11 @@
+package com.cursee.monolib.core;
+
+@Deprecated(since = "2.0.0", forRemoval = true)
+public class MonoLibConfiguration {
+
+    @Deprecated
+    public static boolean debugging = false;
+
+    @Deprecated
+    public static boolean enableAntiRepostingCheck = true;
+}
