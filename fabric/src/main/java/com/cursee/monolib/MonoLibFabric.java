f71e2863f4043943ce973740bdeef92ef995677c
jason13official <jason13official@gmail.com>
2025-01-22 23:10:37 -0500

    command

--- fabric/src/main/java/com/cursee/monolib/MonoLibFabric.java
+++ fabric/src/main/java/com/cursee/monolib/MonoLibFabric.java
@@ -1,11 +1,14 @@
 package com.cursee.monolib;
 
-import com.cursee.monolib.core.command.MonoLibCommand;
+import com.cursee.monolib.core.command.MonoLibCommands;
+import com.cursee.monolib.core.command.hand.HandArgument;
 import com.cursee.monolib.core.registry.RegistryFabric;
 import com.cursee.monolib.core.sailing.Sailing;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.ArgumentTypeRegistry;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.minecraft.resources.ResourceLocation;
 
 public class MonoLibFabric implements ModInitializer {
     
@@ -14,7 +17,8 @@ public class MonoLibFabric implements ModInitializer {
         MonoLib.init();
         Sailing.register(Constants.MOD_ID, Constants.MOD_NAME, Constants.MOD_VERSION, Constants.MOD_PUBLISHER, Constants.MOD_URL);
         ServerEntityEvents.ENTITY_LOAD.register(Sailing::onEntityJoinLevel);
-        CommandRegistrationCallback.EVENT.register(MonoLibCommand::defineCommands);
+        ArgumentTypeRegistry.registerArgumentType(new ResourceLocation(Constants.MOD_ID, "item_output"), HandArgument.class, HandArgument.SERIALIZER);
+        CommandRegistrationCallback.EVENT.register(MonoLibCommands::defineCommands);
         RegistryFabric.register();
     }
 }
--- forge/src/main/java/com/cursee/monolib/MonoLibForge.java
+++ forge/src/main/java/com/cursee/monolib/MonoLibForge.java
@@ -1,23 +1,37 @@
 package com.cursee.monolib;
 
-import com.cursee.monolib.core.command.MonoLibCommand;
+import com.cursee.monolib.core.command.MonoLibCommands;
+import com.cursee.monolib.core.command.hand.HandArgument;
 import com.cursee.monolib.core.registry.RegistryForge;
 import com.cursee.monolib.core.sailing.Sailing;
+import net.minecraft.commands.synchronization.ArgumentTypeInfo;
+import net.minecraft.commands.synchronization.ArgumentTypeInfos;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.entity.EntityJoinLevelEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.RegisterEvent;
+
 import java.util.function.Consumer;
 
 @Mod(Constants.MOD_ID)
 public class MonoLibForge {
+
+    public static IEventBus EVENT_BUS = null;
     
     public MonoLibForge() {
         MonoLib.init();
         Sailing.register(Constants.MOD_ID, Constants.MOD_NAME, Constants.MOD_VERSION, Constants.MOD_PUBLISHER, Constants.MOD_URL);
+        EVENT_BUS = FMLJavaModLoadingContext.get().getModEventBus();
         MinecraftForge.EVENT_BUS.addListener((Consumer<EntityJoinLevelEvent>) event -> Sailing.onEntityJoinLevel(event.getEntity(), event.getLevel()));
-        MinecraftForge.EVENT_BUS.addListener((Consumer<RegisterCommandsEvent>) event -> MonoLibCommand.defineCommands(event.getDispatcher(), event.getBuildContext(), event.getCommandSelection()));
+        MonoLibForge.EVENT_BUS.addListener((Consumer<RegisterEvent>) event -> {
+            if (event.getRegistryKey().equals(Registries.COMMAND_ARGUMENT_TYPE)) event.<ArgumentTypeInfo<?, ?>>register(Registries.COMMAND_ARGUMENT_TYPE, new ResourceLocation(Constants.MOD_ID, "item_output"), () -> ArgumentTypeInfos.registerByClass(HandArgument.class, HandArgument.SERIALIZER));
+        });
+        MinecraftForge.EVENT_BUS.addListener((Consumer<RegisterCommandsEvent>) event -> MonoLibCommands.defineCommands(event.getDispatcher(), event.getBuildContext(), event.getCommandSelection()));
         RegistryForge.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 }
