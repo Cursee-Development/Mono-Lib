0b4a15bcfe75dd2ca497b62cdbf07305953fd3e4
jason13official <jason13official@gmail.com>
2025-01-23 13:11:12 -0500

    Update MonoLibConfiguration.java

--- common/src/main/java/com/cursee/monolib/core/MonoLibConfiguration.java
+++ common/src/main/java/com/cursee/monolib/core/MonoLibConfiguration.java
@@ -1,7 +1,7 @@
 package com.cursee.monolib.core;
 
 @Deprecated(since = "2.0.0", forRemoval = true)
-public class MonoLibConfig {
+public class MonoLibConfiguration {
 
     public static boolean debugging = false;
     public static boolean enableAntiRepostingCheck = true;
